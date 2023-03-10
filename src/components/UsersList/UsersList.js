d3f28530bb7cd55340a010a08f164c2db4af80ff
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-10 13:10:09 +0100

    create example array with students generated by AI, rename App.js on Root, create component with student

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { users } from 'data/users';
 
 const UsersList = () => (
-    <div></div>
+    <div>
+        <ul>
+            {users.map(({name, average, attendance}) =>
+            (
+                <li>
+                    <div>{average}</div>
+                <div>
+                    <p>{name}</p>
+                    <p>{attendance}</p> 
+                </div>
+                <button>X</button>
+                </li>
+            ))}
+        </ul>
+    </div>
 );
 
 UsersList.propTypes = {};
---
+++ src/data/users.js
@@ -0,0 +1,44 @@
+const users = [
+    {
+      name: "John Doe",
+      attendance: "44%",
+      average: "2.3",
+    },
+    {
+      name: "Jane Smith",
+      attendance: "72%",
+      average: "3.7",
+    },
+    {
+      name: "Bob Johnson",
+      attendance: "89%",
+      average: "4.0",
+    },
+    {
+      name: "Alice Lee",
+      attendance: "95%",
+      average: "3.9",
+    },
+    {
+      name: "Tom Jackson",
+      attendance: "60%",
+      average: "2.5",
+    },
+    {
+      name: "Samantha Lee",
+      attendance: "85%",
+      average: "3.8",
+    },
+    {
+      name: "Michael Chen",
+      attendance: "78%",
+      average: "3.5",
+    },
+    {
+      name: "Grace Kim",
+      attendance: "92%",
+      average: "3.9",
+    }
+  ];
+  
+  export {users};
