2145e29a397a399860fefe88d9f6cf3392a42bf3
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-12 15:01:22 +0100

    state

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { users } from 'data/users';
-import UsersListItem from 'components/molecules/UserListItem/UsersListItem.js';
-import styled from 'styled-components';
+import React from "react";
+import { users } from "data/users";
+import UsersListItem from "components/molecules/UserListItem/UsersListItem.js";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   background-color: white;
@@ -10,28 +10,35 @@ const Wrapper = styled.div`
   padding: 40px 30px;
   border-radius: 25px;
   -webkit-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
--moz-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+  -moz-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+  box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
 `;
 
 const StyledList = styled.ul`
-    list-style: none;
-    margin: 0;
-    padding: 0;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
 
+class UsersList extends React.Component {
+  state = {
+    users,
+    title: "Users"
+  };
 
-const UsersList = () => (
-    <Wrapper>
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.title}</h1>
+        <button onClick={() => this.setState({title: 'random'})}>Change</button>
         <StyledList>
-            {users.map((userData, i) =>
-            (
-                <UsersListItem index = {i} userData = {userData}/>
-            ))}
+          {users.map((userData, i) => (
+            <UsersListItem index={i} userData={userData} />
+          ))}
         </StyledList>
-    </Wrapper>
-);
-
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
-
